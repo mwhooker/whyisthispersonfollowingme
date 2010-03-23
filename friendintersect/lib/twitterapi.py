5c35fd2cd2f8484c51815ae66f2988085e02c52f
Matthew Hooker <mhooker@digg.com>
2010-03-23 02:54:44 -0400

    new skelleton restkit filter for http caching. breaks twitter classes in to model file and lib file w/ implementations

--- friendintersect/lib/twitterapi.py
+++ friendintersect/lib/twitterapi.py
@@ -1,74 +1,9 @@
-from restkit import Resource, ConnectionPool, BasicAuth
+from restkit import ConnectionPool, BasicAuth
+from friendintersect.model.twitter import *
 from pylons import config
-import json
+
 
 pool = ConnectionPool(max_connections=5)
-
-class TwitterSocialGraph(Resource):
-
-    def __init__(self,  pool_instance=None, **kwargs):
-        friends_url = "http://api.twitter.com/1/"
-        super(TwitterSocialGraph, self).__init__(friends_url, follow_redirect=True,
-                                            max_follow_redirect=10,
-                                            pool_instance=pool_instance,
-                                            **kwargs)
-
-    def GetFollowers(self, id):
-        """TODO: if followers > 5000, this won't work"""
-        followers = self.get('followers/ids/%s.json' % id, cursor=-1)
-        return followers['ids']
-
-    def GetFriends(self, id):
-        """TODO: if friends > 5000, this won't work"""
-        friends = self.get('friends/ids/%s.json' % id, cursor=-1)
-        return friends['ids']
-
-    def request(self, *args, **kwargs):
-        resp = super(TwitterSocialGraph, self).request(*args, **kwargs)
-        return json.loads(resp.body)
-
-
-class TwitterUsers(Resource):
-
-
-    def __init__(self,  pool_instance=None, **kwargs):
-        url = "http://api.twitter.com/1/users/"
-        super(TwitterUsers, self).__init__(url, follow_redirect=True,
-                                            max_follow_redirect=10,
-                                            pool_instance=pool_instance,
-                                            **kwargs)
-    def Lookup(self, ids):
-        def chunks(l, n):
-            """ Yield successive n-sized chunks from l."""
-            for i in xrange(0, len(l), n):
-                yield l[i:i+n]
-
-        #need to break up the requests in to groups of 20 ids
-        id_chunks = list(chunks(ids, 20))
-
-        people = []
-        for ids in id_chunks:
-            people += (self.get('lookup.json', user_id=','.join(map(str,ids))))
-        return people
-
-    def request(self, *args, **kwargs):
-        resp = super(TwitterUsers, self).request(*args, **kwargs)
-        return json.loads(resp.body)
-
-class TwitterRateLimit(Resource):
-    
-
-    def __init__(self, pool_instance=None, **kwargs):
-        url = "http://api.twitter.com/1/account/rate_limit_status.json"
-        super(TwitterRateLimit, self).__init__(url, follow_redirect=True,
-                                            max_follow_redirect=10,
-                                            pool_instance=pool_instance,
-                                            **kwargs)
-    
-    def request(self, *args, **kwargs):
-        resp = super(TwitterRateLimit, self).request(*args, **kwargs)
-        return json.loads(resp.body)
-
 auth = BasicAuth(config.get('twitter.username'), config.get('twitter.password'))
 
 peeps = TwitterUsers(pool, filters=[auth])
