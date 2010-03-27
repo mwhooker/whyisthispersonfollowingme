280aa7b84d612abc8d1d41f5ea5cd07aad65066a
Matthew Hooker <mhooker@digg.com>
2010-03-27 17:35:07 -0400

    use http cache from restkit

--- friendintersect/lib/twitterapi.py
+++ friendintersect/lib/twitterapi.py
@@ -1,11 +1,13 @@
 from restkit import ConnectionPool, BasicAuth
 from friendintersect.model.twitter import *
+from restkit.http_cache import HttpCache
 from pylons import config
 
 
 pool = ConnectionPool(max_connections=5)
 auth = BasicAuth(config.get('twitter.username'), config.get('twitter.password'))
+cache = HttpCache()
 
-peeps = TwitterUsers(pool, filters=[auth])
-thesocial = TwitterSocialGraph(pool)
+peeps = TwitterUsers(pool, filters=[auth, cache])
+thesocial = TwitterSocialGraph(pool, filters=[cache])
 nolimits = TwitterRateLimit(pool)
